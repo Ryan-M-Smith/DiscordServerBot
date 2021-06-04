599bb3dee2bbb36e43f643076b717302345f6773
Ryan Smith <rysmith2113@gmail.com>
2021-06-04 00:38:22 -0400

    Add a place for command parameters

---
+++ src/params.py
@@ -0,0 +1,15 @@
+#
+# FILENAME: params.py | ServerBot
+# DESCRIPTION: Parameter lists (list of dictionaries) for commands
+# CREATED: 2021-06-04 @ 12:23 AM
+# COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+#
+
+PING_OPTIONS = [
+	{
+		"name": "times",
+		"description": "The number of times to ping",
+		"required": False,
+		"type": 3
+	}
+]
