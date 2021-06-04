81304179c3f373bc124a21852a4d0b90e471f5da
Ryan Smith <69333713+Ryan-M-Smith@users.noreply.github.com>
2021-06-04 10:12:43 -0400

    Add a /info command

--- src/commands.py
+++ src/commands.py
@@ -10,11 +10,13 @@ import os, discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext, SlashCommandOptionType
 from dotenv import load_dotenv
+from datetime import datetime
 
 from params import *
 
 load_dotenv("../.env")
 TOKEN = os.getenv("DISCORD_TOKEN")
+SERVER = os.getenv("DISCORD_SERVER")
 
 client = commands.Bot(command_prefix="/")
 slash = SlashCommand(client)
@@ -30,5 +32,25 @@ async def ping(ctx: SlashCommand, times=1):
 	"""
 	for _ in range(times):
 		await ctx.send(content=f"Query recieved ({round(client.latency * 1000)} ms)")
+
+@client.command(
+	name="info", description="Get info about the server.",
+	guild_ids=[
+		850104153278644305, 850104153278644307, 850116523381096448,
+		850104800188170280, 850145316530683946
+	],
+	options=None
+)
+async def info(ctx: SlashCommand):
+	"""
+		Get info about the server.
+		Usage: `/info`
+	"""
+	time, date = datetime.now(), datetime.date()
+	
+	await ctx.send(content=(
+		f"Welcome to the {SERVER} server!" + \
+		f"It is currently {date.month}/{date.day}/{date.year} at {time.strftime("%I:%M %p")[1:]}"
+	))
 
 client.run(TOKEN)
